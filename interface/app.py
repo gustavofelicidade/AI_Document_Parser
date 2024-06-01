b0769d13049eda0c87f98d8c9bf9896a80a887b9
gustavofelicidade <gustavosnik95@gmail.com>
2024-06-01 19:28:48 -0300

    Instantiate OpenAI client with API key and update interface content.

--- interface/app.py
+++ interface/app.py
@@ -9,9 +9,14 @@ import pandas as pd
 import json
 from collections import defaultdict
 import yaml
+from openai import OpenAI
 from yaml.loader import SafeLoader
 
+# Load the OPENAI_API_KEY from your environment variables
+api_key = os.getenv("OPENAI_API_KEY")
 
+# Instantiate the OpenAI client with your API key
+client = OpenAI(api_key=api_key)
 
 dotenv.load_dotenv()
 
@@ -22,15 +27,15 @@ def main_page():
 
     col1, col2 = st.columns([10, 4])
     with col2:
-        st.markdown("# Getting Start")
-        st.subheader('Know how to Scrap')
+        st.markdown("# Document Intelligence")
+        st.subheader('...')
         ...
     with col1:
-        st.markdown("# SNB 🚀")
+        st.markdown("# AI Vision 👁️")
         # st.subheader('URL Manager ')
-        st.write("Follow the next steps to URL process:")
+        st.write("Follow the next steps extract data from the documents:")
 
-    st.markdown("## URL's")
+
     st.warning(
         """ \n Insert the URL \n
                 Click add url \n
@@ -44,65 +49,10 @@ def main_page():
     st.markdown("""---""")
 
 
-    st.markdown("## URL Elements")
-    st.text('All content available')
-    # st.text('Select the element and click to hide  rows with empty cells')
-
-    # selected_elements = display_all_elements()
-    #
-    # st.dataframe(selected_elements)
-
-    st.text("Select elements by tag name")
-    # st.dataframe(tag_name)
-    st.markdown("""---""")
-
-
-
-    st.markdown("""Generate Post""")
-
-    # ===========================================================
-    # Section to append data to Google Sheets
-    # ===========================================================
-    st.markdown("### Post Data to Google Sheets")
-    if st.button('Post to Google Sheets'):
-        # try:
-
-
-
-        # Add prompts to openai before append
-
-
-
-        st.success("Data posted successfully to Google Sheets.")
-        # except gspread.exceptions.APIError as e:
-        #     if e.response.status_code == 429:
-        #         st.error("Quota limit reached for Google Sheets API. Please input less content.")
-        #     else:
-        #         st.error(f"Failed to post data to Google Sheets. Error: {e}")
-
-    # ===========================================================
 
     st.markdown("""---""")
 
-    # ===========================================================
-    # Section to post data to WordPress
-    # ===========================================================
-    st.markdown("### Post Data to WordPress")
-    wordpress_url = st.text_input("WordPress Website URL:", key="wordpress_url")
-    content_cell = st.text_input("Cell Coordinate for Content (e.g., 'A1', 'D23'):", key="content_cell")
-    if st.button('Submit to WordPress'):
-        # Here you would include the logic to post data to WordPress
-        # For example, you might retrieve the content from Google Sheets using the content_cell coordinate
-        # and then use the WordPress API to post the content to the wordpress_url
-        st.success("Content submitted to WordPress.")
-    st.markdown("""---""")
 
-
-    st.markdown("## URL's Sitemap")
-    st.text("URL's in this site")
-
-
-    st.markdown("""---""")
 
 
     st.markdown("""Footer""")
