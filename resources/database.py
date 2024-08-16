20dbb0efa5397a50765007e574ee2fc96b2da249
gustavofelicidade <gustavosnik95@gmail.com>
2024-08-16 15:42:30 -0300

    Adding Postgres Database 0.0.0.4

--- resources/database.py
+++ resources/database.py
@@ -21,14 +21,32 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Função para inicializar a conexão com o banco de dados
+# def init_connection():
+#     return psycopg2.connect(
+#         host=os.getenv("DB_HOST"),
+#         port=os.getenv("DB_PORT"),
+#         dbname=os.getenv("DB_NAME"),
+#         user=os.getenv("DB_USER"),
+#         password=os.getenv("DB_PASSWORD"),
+#     )
+
+
+# conn = init_connection()
+# cur = conn.cursor()
+#
+# # Função para inserir o caminho e os dados da imagem no banco de dados
+# def insert_image(file_path, file_data):
+#     cur.execute("""
+#         INSERT INTO public.users (file_path, file_data)
+#         VALUES (%s, %s)
+#     """, (file_path, psycopg2.Binary(file_data)))
+#     conn.commit()
+
+
+# RAILWAY Postgres Database
+# Função para inicializar a conexão com o banco de dados usando a URL completa
 def init_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-    )
+    return psycopg2.connect(os.getenv("DATABASE_PUBLIC_URL"))
 
 conn = init_connection()
 cur = conn.cursor()
